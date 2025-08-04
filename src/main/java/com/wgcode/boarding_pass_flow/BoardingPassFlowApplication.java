26966cb5f5b0658ba89866dd47229fe8e737225f
gfizzle <winstongdouglas4@gmail.com>
2025-08-03 21:57:29 -0500

    feat: First commit of base project build

---
+++ src/main/java/com/wgcode/boarding_pass_flow/BoardingPassFlowApplication.java
@@ -0,0 +1,13 @@
+package com.wgcode.boarding_pass_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardingPassFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardingPassFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wgcode/boarding_pass_flow/BoardingPassFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgcode.boarding_pass_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardingPassFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
